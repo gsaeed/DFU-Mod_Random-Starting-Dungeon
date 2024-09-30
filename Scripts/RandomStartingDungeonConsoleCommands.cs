f9154750e466afd644c688bef1726e55d8018de4
George.Saeed <gsaeed@yahoo.com>
2024-09-30 16:00:12 +0100

    added create_location_database to dump all locations and there position and type to a file

--- Scripts/RandomStartingDungeonConsoleCommands.cs
+++ Scripts/RandomStartingDungeonConsoleCommands.cs
@@ -10,8 +10,11 @@
 
 using DaggerfallWorkshop.Game;
 using System;
+using System.IO;
+using DaggerfallWorkshop;
 using UnityEngine;
 using Wenzil.Console;
+using DaggerfallConnect.Arena2;
 
 namespace RandomStartingDungeon
 {
@@ -26,6 +29,7 @@ namespace RandomStartingDungeon
                 ConsoleCommandsDatabase.RegisterCommand(ManualRandomTeleport.name, ManualRandomTeleport.description, ManualRandomTeleport.usage, ManualRandomTeleport.Execute);
                 ConsoleCommandsDatabase.RegisterCommand(TransformDungPos.name, TransformDungPos.description, TransformDungPos.usage, TransformDungPos.Execute);
                 ConsoleCommandsDatabase.RegisterCommand(CurrentBlockInfo.name, CurrentBlockInfo.description, CurrentBlockInfo.usage, CurrentBlockInfo.Execute);
+                ConsoleCommandsDatabase.RegisterCommand(CreateLocationDatabase.name, CreateLocationDatabase.description, CreateLocationDatabase.usage, CreateLocationDatabase.Execute);
             }
             catch (Exception e)
             {
@@ -51,6 +55,51 @@ namespace RandomStartingDungeon
             }
         }
 
+        private static class CreateLocationDatabase
+        {
+            public static readonly string name = "create_location_database";
+            public static readonly string description = "Builds location database";
+            public static readonly string usage = "Takes no params";
+
+            public static string Execute(params string[] args)
+            {
+                var filePath = Application.persistentDataPath + @"/AllTheTowns.txt";
+                try
+                {
+                    // Check if the file exists and delete it if it does
+                    if (File.Exists(filePath))
+                    {
+                        File.Delete(filePath);
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.WriteLine($"Region Name\tRegion ID\tLocation Index\tLocation Name\tLocation ID\tLocation Key\tLocation Type\tDungeon Type\tLatitude\tLongitude\tMapPixel X\tMapPixel Y");
+
+                        for (int n = 0; n < 62; n++)
+                        {
+                            var regionData = DaggerfallUnity.Instance.ContentReader.MapFileReader.GetRegion(n);
+                            for (int i = 0; i < regionData.LocationCount; i++)
+                            {
+                                var location = regionData.MapTable[i];
+                                var dfloc = DaggerfallUnity.Instance.ContentReader.MapFileReader.GetLocation(n, i);
+                                var mapPixel = MapsFile.LongitudeLatitudeToMapPixel(location.Longitude, location.Latitude);
+                                var str = $"{regionData.Name}\t{n}\t{i}\t{dfloc.Name}\t{location.MapId}";
+                                str += $"\t{dfloc.MapTableData.Key}\t{location.LocationType.ToString()}\t{location.DungeonType.ToString()}";
+                                str += $"\t{location.Latitude}\t{location.Longitude}\t{mapPixel.X}\t{mapPixel.Y}";
+                                writer.WriteLine($"{str}");
+                            }
+                        }
+                    }
+
+                    return $"Location database successfully written to {filePath}.";
+                }
+                catch (Exception e)
+                {
+                    return $"Error writing location database: {e.Message}";
+                }
+            }
+        }
         private static class TransformDungPos
         {
             public static readonly string name = "transform_dung_pos";
